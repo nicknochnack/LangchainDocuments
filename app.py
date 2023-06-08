edfc5d5515d6cb9418bc7b61d92a3d945fe7461e
Nicholas Renotte <nicholas.renotte@gmail.com>
2023-06-09 08:39:18 +1000

    Updated embeddings component

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import os
 # Import OpenAI as main LLM service
 from langchain.llms import OpenAI
+from langchain.embeddings import OpenAIEmbeddings
 # Bring in streamlit for UI/app interface
 import streamlit as st
 
@@ -19,17 +20,18 @@ from langchain.agents.agent_toolkits import (
 
 # Set APIkey for OpenAI Service
 # Can sub this out for other LLM providers
-os.environ['OPENAI_API_KEY'] = 'yourapikeyhere'
+os.environ['OPENAI_API_KEY'] = 'youropenaiapikeyhere'
 
 # Create instance of OpenAI LLM
 llm = OpenAI(temperature=0.1, verbose=True)
+embeddings = OpenAIEmbeddings()
 
 # Create and load PDF Loader
 loader = PyPDFLoader('annualreport.pdf')
 # Split pages from pdf 
 pages = loader.load_and_split()
 # Load documents into vector database aka ChromaDB
-store = Chroma.from_documents(pages, collection_name='annualreport')
+store = Chroma.from_documents(pages, embeddings, collection_name='annualreport')
 
 # Create vectorstore info object - metadata repo?
 vectorstore_info = VectorStoreInfo(
